34c04503cce4b7ef22a64cdf8c596db964e3fda7
Zuzana K <zk0433@gmail.com>
2017-06-06 21:42:27 +0100

    js refactored to use a forEach loop

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 $(document).ready(function(){
 
+
+
 	/* Take the value of the input from each field (use the id)
 		and save it into a variable.
-	*/
-	$("#formOne").submit(function(event){
+	
+	$("#blanks form").submit(function(event){
 		var person1Input = $('input#person1').val();
 		var person2Input = $('input#person2').val();
 		var animalInput = $('input#animal').val();
@@ -14,7 +16,7 @@ $(document).ready(function(){
 	/*
 		Take the variable and append it to the span 
 		class in the story.
-	*/
+	
 		$(".person1").text(person1Input);
 		$(".person2").text(person2Input);
     	$(".animal").text(animalInput);
@@ -29,5 +31,31 @@ $(document).ready(function(){
 	    	
 	    	event.preventDefault();
 	}); // formOne
+
+	*/
+
+	$("#blanks form").submit(function(event) {
+		// create an array with all the input id names
+		var blanks = ["person1", "person2", "animal", "exclamation", "verb", "noun"];
+
+		// run a forEach loop for each of the array item
+		blanks.forEach(function(blank) {
+			// create a general variable userInput that will
+			// take the value from the input + id name
+			// for each of the array elements
+			// something like: var person1Input = $('#person1').val();
+			var userInput = $("#" + blank).val();
+			// and take that value and append it with text()
+			// to the class in the story
+			$("." + blank).text(userInput);
+			// The loop will do this for every element of the array
+
+		});
+
+		$("#story").show();
+
+		event.preventDefault();
+	});
+
 
 }); // document.ready
